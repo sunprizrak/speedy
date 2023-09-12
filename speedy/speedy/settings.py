cda2c6e65f40d21dbc85ced114c9ad0c338f7138
sunprizrak <sunprizrak@gmail.com>
2023-09-12 16:05:48 +0300

    ...

--- speedy/speedy/settings.py
+++ speedy/speedy/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(int(os.environ.get('DEBUG', '1'))),
+DEBUG = bool(int(os.environ.get('DEBUG', '1')))
 
 ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '*').split(' ')
 
